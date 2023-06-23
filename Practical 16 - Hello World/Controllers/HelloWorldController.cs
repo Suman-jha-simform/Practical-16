63f5abb301e3d4a2338466f2aae52dbb93abb086
suman jha <suman.j@simformsolutions.com>
2023-06-23 12:13:29 +0530

    added practical 16

---
+++ Practical 16 - Hello World/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Practical_16___Hello_World.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+        private readonly ILogger<HelloWorldController> _logger;
+
+        public HelloWorldController(ILogger<HelloWorldController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpGet]
+        // api/HelloWorld
+        public IActionResult Get()
+        {
+            _logger.LogInformation("Hello World api is called.");
+            return Ok("Hello world");
+        }
+    }
+}
